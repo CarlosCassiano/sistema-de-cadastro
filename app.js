246b5b9d6974a0f05abeab3cb9337defd817c106
Carlos Junior <juniorcassianors@hotmil.com>
2024-06-27 20:56:27 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação está se conectando ao banco de dados");
 // mais um console log de teste://
-console.log("O paulo fes esse commit sem voce ver na aula");
+console.log("O paulo fez esse commit sem voce ver na aula");
 
